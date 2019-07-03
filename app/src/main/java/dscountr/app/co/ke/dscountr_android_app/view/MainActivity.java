90e8f40283d9d1a9d009b22dc21149418db535a2
Imaya <dismas@nostra.co.ke>
2019-07-03 10:22:40 +0300

    initialized the app architecture pattern

--- app/src/main/java/dscountr/app/co/ke/dscountr_android_app/view/MainActivity.java
+++ app/src/main/java/dscountr/app/co/ke/dscountr_android_app/view/MainActivity.java
@@ -1,7 +1,9 @@
-package dscountr.app.co.ke.dscountr_android_app;
+package dscountr.app.co.ke.dscountr_android_app.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import dscountr.app.co.ke.dscountr_android_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
