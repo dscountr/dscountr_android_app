ffd5e3b9b8c1a52acc552d653bf057e9fb54f060
Imaya <dismas@nostra.co.ke>
2019-07-04 17:17:02 +0300

    finished the design of the gender layout

--- app/src/main/java/dscountr/app/co/ke/dscountr_android_app/view/registration/fragments/GenderFragment.java
+++ app/src/main/java/dscountr/app/co/ke/dscountr_android_app/view/registration/fragments/GenderFragment.java
@@ -1,21 +1,72 @@
 package dscountr.app.co.ke.dscountr_android_app.view.registration.fragments;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import dscountr.app.co.ke.dscountr_android_app.R;
 
-public class GenderFragment extends Fragment {
+public class GenderFragment extends Fragment implements Toolbar.OnMenuItemClickListener, Button.OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View gender = inflater.inflate(R.layout.fragment_gender, container, false);
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(this);
+        Button btnGender = gender.findViewById(R.id.btnGender);
+        btnGender.setOnClickListener(this);
+
+        LinearLayout llVerifyBack = gender.findViewById(R.id.llVerifyBack);
+        llVerifyBack.setOnClickListener(this);
 
         return gender;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // create a FragmentManager
+        FragmentManager fragmentManager = getFragmentManager();
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit(); // save the changes
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.miRegistrationHelp:
+                Toast.makeText(getActivity(), "Gender help.",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnGender:
+                loadFragment(new DateOfBirthFragment());
+                break;
+            case R.id.llVerifyBack:
+                Toast.makeText(getActivity(), "You clicked the back button.",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+
+        }
     }
 
     @Override
