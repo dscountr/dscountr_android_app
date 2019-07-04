69e793fe6a61eb43124954258f282af93ef941e2
Imaya <dismas@nostra.co.ke>
2019-07-04 12:38:11 +0300

    finished working on the design of the email address layout

--- app/src/main/java/dscountr/app/co/ke/dscountr_android_app/view/registration/fragments/EmailFragment.java
+++ app/src/main/java/dscountr/app/co/ke/dscountr_android_app/view/registration/fragments/EmailFragment.java
@@ -1,21 +1,71 @@
 package dscountr.app.co.ke.dscountr_android_app.view.registration.fragments;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import dscountr.app.co.ke.dscountr_android_app.R;
 
-public class EmailFragment extends Fragment {
+public class EmailFragment extends Fragment implements Toolbar.OnMenuItemClickListener, Button.OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View email = inflater.inflate(R.layout.fragment_email, container, false);
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(this);
+        Button btnEmail = email.findViewById(R.id.btnEmail);
+        TextView tvWhyEmail = email.findViewById(R.id.tvWhyEmail);
+        btnEmail.setOnClickListener(this);
+        tvWhyEmail.setOnClickListener(this);
 
         return email;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // create a FragmentManager
+        FragmentManager fragmentManager = getFragmentManager();
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit(); // save the changes
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.miRegistrationHelp:
+                Toast.makeText(getActivity(), "Email address help.",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnEmail:
+                loadFragment(new VerifyEmailFragment());
+                break;
+            case R.id.tvWhyEmail:
+                Toast.makeText(getActivity(), "Why do we need your email?",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+
+        }
     }
 
     @Override
