f92ad84faf4e4a0505b97aab287c576d9ff8b26e
ImayaDismas <imayadismas@gmail.com>
2019-07-06 18:08:43 +0300

    finished designing the main page menu items

--- app/src/main/java/dscountr/app/co/ke/dscountr_android_app/view/MainActivity.java
+++ app/src/main/java/dscountr/app/co/ke/dscountr_android_app/view/MainActivity.java
@@ -2,6 +2,7 @@ package dscountr.app.co.ke.dscountr_android_app.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import dscountr.app.co.ke.dscountr_android_app.R;
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.main_menu);
     }
 }
